5d0ee6487c61134f3ef7197813f8909d9d32bac5
WangYG <wangyonggang@ctyun.cn>
2016-01-26 12:01:31 +0800

    update demo

--- base-demo/index.jsx
+++ base-demo/index.jsx
@@ -1,14 +1,36 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import { DatePicker } from 'antd';
+let ReactDOM = mimiron2.ReactDOM;
+let React = mimiron2.React;
+const DatePicker = mimiron2.DatePicker;
+const Button = mimiron2.Button;
+const Collapse = mimiron2.Collapse;
+const Panel = Collapse.Panel;
 
-const App = React.createClass({
-  render() {
-    return (
-      <DatePicker />
-    );
-  },
-});
+function callback(key) {
+  console.log(key);
+}
+
+const text = "i am text";
 
 
-ReactDOM.render(<App />, document.getElementById('react-content'));
+ReactDOM.render(
+  <div>Mimiron2
+    <DatePicker />
+    <Button type="primary">主按钮</Button>
+    <h1>Hello, world!</h1>
+    <Collapse defaultActiveKey={['1']} onChange={callback}>
+      <Panel header="This is panel header 1" key="1">
+        <p>{text}</p>
+      </Panel>
+      <Panel header="This is panel header 2" key="2">
+        <p>{text}</p>
+      </Panel>
+      <Panel header="This is panel header 3" key="3">
+        <p>{text}</p>
+      </Panel>
+    </Collapse>
+  </div>,
+  document.getElementById('main')
+);
+
+let a =1;
+alert(a);
